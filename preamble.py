8e4e98e59d2a23d8c0becc3c353f503b7d310b96
Noah Ströhle <drnochi.dev@gmail.com>
2019-09-21 11:42:23 -0700

    Fix future/deprecation errors and warnings

--- preamble.py
+++ preamble.py
@@ -21,4 +21,6 @@ np.set_printoptions(precision=3, suppress=True)
 pd.set_option("display.max_columns", 8)
 pd.set_option('precision', 2)
 
+pd.plotting.register_matplotlib_converters()
+
 __all__ = ['np', 'mglearn', 'display', 'plt', 'pd']
